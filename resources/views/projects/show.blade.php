c5cfd54868169e143d390335584aa70480913d1f
mdaskalov <thefallenangel.bg@gmail.com>
2019-07-13 19:56:01 +0800

    Lesson 11

--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -2,17 +2,71 @@
 
 @section('content')
     {{--{{$projects}}--}}
+    <div class="row">
+        <div class="col-lg-10">
+            <a href="/projects">
+                Home
+            </a>
+            / {{$project->title}}
+        </div>
+        <div class="col-lg-2">
+            <a href="/projects" class="float-right">
+                <button type="button" class="btn btn-primary">
+                    Back
+                </button>
+            </a>
+        {{--</div>--}}
+        </div>
+    </div>
 
-    {{--<div class="container-fluid">--}}
-    {{--@foreach($projects as $project)--}}
-        {{$project->title}} <br>
-        {{$project->description}}
-    {{--@endforeach--}}
 
-        <a href="/projects" class="float-right">
-            <button type="button" class="btn btn-primary">
-                Back
-            </button>
-        </a>
-    {{--</div>--}}
+
+    <div class="row project">
+        <div class="col-lg-10">
+            <div>
+                <h2>Tasks:</h2>
+                {{--@foreach($tasks as $key => $task)--}}
+                <div id="task-0" class="card rounded-1 shadow bbtask">
+                    <div class="card-body">
+                        simple task
+                    </div>
+                </div>
+
+                {{--@endforeach--}}
+
+            </div>
+
+            <div>
+                <h2>Notes:</h2>
+                <div id="notes" class="card rounded-1 shadow bbtask">
+                    <div class="card-body">
+                        Notes he
+                        dsaklnfsd
+                        asfdasfadkl;f <br>
+                        sdfdsf<br>
+                        sdasdf<br>
+                        asdfafas
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-lg-2">
+            <h2>Project Info</h2>
+            <div>
+                <div class="card bbcard rounded-1 shadow bg-white">
+                    <div class="card-header bg-transparent border border-primary border-bottom-0 border-3">
+                        <h5>{{$project->title}}</h5>
+                    </div>
+                    <div class="card-body">
+                        <p class="card-text">{{ $project->description }}</p>
+                    </div>
+                    <div class="card-footer">
+                        <small class="text-muted float-center">Last updated<br>{{ $project->last_updated }}</small>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
 @endsection
