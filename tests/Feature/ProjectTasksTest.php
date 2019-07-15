84897cf1721eaf2f8e534111231d92ea6dd20b4e
mdaskalov <thefallenangel.bg@gmail.com>
2019-07-16 00:10:54 +0800

    Lesson 17

--- tests/Feature/ProjectTasksTest.php
+++ tests/Feature/ProjectTasksTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Feature;
 
 use App\Project;
+use Tests\Setup\ProjectFactory;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
@@ -15,12 +16,17 @@ class ProjectTasksTest extends TestCase
     public function a_project_can_have_tasks()
     {
         $this->withoutExceptionHandling();
-        $this->signIn();
+//        $this->signIn();
+//
+//        $project = factory(Project::class)->create(['owner_id' => auth()->id()]);
 
-        $project = factory(Project::class)->create(['owner_id' => auth()->id()]);
+        $project = app(ProjectFactory::class)
+            ->ownedBy($this->signIn())
+//            ->withTasks(1)
+            ->create();
         
         $this->post($project->path() . '/tasks', ['body' => 'Test Task', 'user_id' => auth()->id()]);
-//dd('wwaat');
+
         $this->get($project->path())
             ->assertSee('Test Task');
     }
@@ -39,9 +45,16 @@ class ProjectTasksTest extends TestCase
     public function a_task_requires_a_body()
     {
 //        $this->withoutExceptionHandling();
-        $this->signIn();
-        $project = factory(Project::class)->create(['owner_id' => auth()->id()]);
+//        $this->signIn();
+//        $project = factory(Project::class)->create(['owner_id' => auth()->id()]);
+
+        $project = app(ProjectFactory::class)
+            ->ownedBy($this->signIn())
+//            ->withTasks(1)
+            ->create();
+
         $attributes = factory('App\Task')->raw(['body' => '']);
+
         $this->post($project->path() . '/tasks', $attributes)->assertSessionHasErrors('body');
     }
 
@@ -62,14 +75,18 @@ class ProjectTasksTest extends TestCase
     public function only_the_owner_of_a_project_may_update_tasks()
     {
         $this->signIn();
-        $project = factory('App\Project')->create();
-        $task = $project->addTask([
-            'body' => 'Test Task',
-            'completed' => false,
-            'user_id' => auth()->id()
-        ]);
+//        $project = factory('App\Project')->create();
+//        $task = $project->addTask([
+//            'body' => 'Test Task',
+//            'completed' => false,
+//            'user_id' => auth()->id()
+//        ]);
+        $project = app(ProjectFactory::class)
+//            ->ownedBy($this->signIn())
+            ->withTasks(1)
+            ->create(false);
 
-        $this->patch($task->path(), ['body' => 'Test Task 123'])
+        $this->patch($project->tasks->first()->path(), ['body' => 'Test Task 123'])
             ->assertStatus(403);
 
         // Must nbot have record in the DB
@@ -79,25 +96,32 @@ class ProjectTasksTest extends TestCase
     /** @test */
     public function a_task_can_be_updated()
     {
-        $this->withoutExceptionHandling();
+//        $this->withoutExceptionHandling();
 
-        $this->signIn();
+        $project = app(ProjectFactory::class)
+            ->ownedBy($this->signIn())
+            ->withTasks(1)
+            ->create();
+
+
+//        $this->signIn();
 //        $project = factory('App\Project')->create();
 
-        // Create a project for the logged in user.
-        $project = auth()->user()->projects()->create(
-            factory(Project::class)->raw()
-        );
-
-        // Add task to the project
-        $task = $project->addTask([
-            'body' => 'Test Task',
-            'completed' => false,
-            'user_id' => auth()->id()
-        ]);
+//        // Create a project for the logged in user.
+//        $project = auth()->user()->projects()->create(
+//            factory(Project::class)->raw()
+//        );
+//
+//        // Add task to the project
+//        $task = $project->addTask([
+//            'body' => 'Test Task',
+//            'completed' => false,
+//            'user_id' => auth()->id()
+//        ]);
 
         // Then try to update without specifying user.
-        $this->patch($task->path(), [
+        // $this->actingAs($project->owner)->patch
+        $this->patch($project->tasks->first()->path(), [
             'body' => 'changed',
             'completed' => true
         ]);
