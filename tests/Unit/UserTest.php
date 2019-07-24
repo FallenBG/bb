a16fe970b07aac105d733ca8e61416dfed4f13f5
mdaskalov <thefallenangel.bg@gmail.com>
2019-07-25 06:46:44 +0800

    Lesson 31

--- tests/Unit/UserTest.php
+++ tests/Unit/UserTest.php
@@ -2,7 +2,9 @@
 
 namespace Tests\Unit;
 
+use App\User;
 use Illuminate\Database\Eloquent\Collection;
+use Tests\Setup\ProjectFactory;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
@@ -11,10 +13,30 @@ class UserTest extends TestCase
 {
     use RefreshDatabase, WithFaker;
     /** @test */
-    public function testExample()
+    public function a_user_has_objects()
     {
         $user = factory('App\User')->create();
 
         $this->assertInstanceOf(Collection::class, $user->projects);
+    }
+    
+    /** @test */
+    public function a_user_has_accessible_projects()
+    {
+        $user = $this->signIn();
+        app(ProjectFactory::class)->ownedBy($user)->create();
+
+        $this->assertCount(1, $user->accessibleProjects());
+
+        $user2 = factory(User::class)->create();
+        $user3 = factory(User::class)->create();
+
+        $project = tap(app(ProjectFactory::class)->ownedBy($user2)->create())->invite($user3);
+
+        $this->assertCount(1, $user->accessibleProjects());
+
+        $project->invite($user);
+
+        $this->assertCount(2, $user->accessibleProjects());
     }
 }
